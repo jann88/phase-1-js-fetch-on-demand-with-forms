3d1f7d8cfad1647cbecadd9b102390b37ceb10e8
Janet Ngei <janet.ngei@student.moringaschool.com>
2022-06-15 11:55:46 +0300

    completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
-const init = () => {
-  
-}
+const initForm = () => {
 
-document.addEventListener('DOMContentLoaded', init);
+
+        document.addEventListener('DOMContentLoaded', init);
+        const inputForm = document.querySelector('form');
+        inputForm.addEventListener('submit', (event) => {
+                event.preventDefault();
+                console.log(event);
+                const input = document.querySelector('input# searchById ');
+                console.log(input.value)
+                fetch(`http://localhost:3000/movies/${input.value}`)
+                    .then(resp => resp.json())
+                    .then(data => {
+                        console.log(data);
+
+                    });
+            }
+        }
+        document.addEventListener('DOMContentLoaded', init);
